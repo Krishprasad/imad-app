987c17af99eed5ffa44732e5042dffe9875ec3b9
Krishprasad <30659219+Krishprasad@users.noreply.github.com>
2017-08-13 11:26:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element=document.getElementById("one");
 element.innerHTML="new Change";
 //move image
 var img=document.getElementById("pic");
-img.onClick=function(){
+img.onclick=function(){
     img.style.marginLeft="100px";
 };
