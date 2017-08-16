fc2738ed61f4552c289786aa0cc38a1107973d62
Krishprasad <30659219+Krishprasad@users.noreply.github.com>
2017-08-16 13:49:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,14 @@ var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
     //make request to the server and send the name
+     var request=new XMLHttpRequest();
+    //capture the response and store it as a variable
+    request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        //take some action
+    if(request.status===200) {
     //capture the list of names and render it as list
-    var names=['name1', 'name2', 'name3','name4'];
+    var names=request.responseText;
     var list="";
     for(var i=0;i<names.length;i++){
         list+="<li>" +names[i]+ "</li>";
@@ -35,3 +41,8 @@ submit.onclick=function(){
     var ul=document.getElementById("namelist");
     ul.innerHTML=list;
 }
+}
+};
+request.open('GET','http://shreekrishprasad.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+}    
